c8edb8214fe551134e53f82bac368365dda5f312
Bilal Dilbar <bilal.dilbar54@gmail.com>
2023-02-01 18:49:31 -0500

    added project files for ICE 3

--- MAPD724-W2023-ICE 3/GameViewController.swift
+++ MAPD724-W2023-ICE 3/GameViewController.swift
@@ -1,55 +1,35 @@
-//
-//  GameViewController.swift
-//  MAPD724-W2023-ICE 3
-//
-//  Created by Muhammad Bilal Dilbar on 2023-02-01.
-//
-
 import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
+class GameViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
+        if let scene = GKScene(fileNamed: "GameScene")
+        {
             // Get the SKScene from the loaded GKScene
             if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
                 // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
-                
                 // Present the scene
-                if let view = self.view as! SKView? {
+                if let view = self.view as! SKView?
+                {
                     view.presentScene(sceneNode)
-                    
                     view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
                 }
             }
         }
     }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask
+    {
+        return .portrait
     }
 
-    override var prefersStatusBarHidden: Bool {
+    override var prefersStatusBarHidden: Bool
+    {
         return true
     }
 }
