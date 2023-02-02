370fae6f8f7b55cd62a110aa1586cbcea7c2ff82
Bilal Dilbar <bilal.dilbar54@gmail.com>
2023-02-01 20:23:13 -0500

    added Sc re and Lives

--- MAPD724-W2023-ICE 3/GameViewController.swift
+++ MAPD724-W2023-ICE 3/GameViewController.swift
@@ -4,6 +4,10 @@ import GameplayKit
 
 class GameViewController: UIViewController
 {
+    @IBOutlet weak var LivesLabel: UILabel!
+    
+    @IBOutlet weak var ScoreLabel: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -21,6 +25,12 @@ class GameViewController: UIViewController
                 }
             }
         }
+        
+        CollisionManager.gameViewController = self
+        ScoreManager.Score = 0
+        ScoreManager.Lives = 5
+        updateLivesLabel()
+        updateScoreLabel()
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask
@@ -31,5 +41,15 @@ class GameViewController: UIViewController
     override var prefersStatusBarHidden: Bool
     {
         return true
+    }
+    
+    func updateLivesLabel()
+    {
+        LivesLabel.text = "Lives: \(ScoreManager.Lives)"
+    }
+    
+    func updateScoreLabel()
+    {
+        ScoreLabel.text = "Score: \(ScoreManager.Score)"
     }
 }
